67befcc169c48c7fa27cb5c1f7500428db9a33b3
Rodchananat K <rod8711@gmail.com>
2020-02-05 16:40:36 +0700

    add basic testing

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -2,12 +2,32 @@ import { Test, TestingModule } from "@nestjs/testing";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 
+import config from 'src/config';
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { User } from "./user/user.entity";
+import { AuthModule } from "./auth/auth.module";
+import { UserModule } from "./user/user.module";
+
 describe("AppController", () => {
   let appController: AppController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
+      imports: [
+        UserModule,
+        AuthModule,
+        TypeOrmModule.forRoot({
+          type: "mysql",
+          host: config.MYSQL_HOST,
+          port: config.MYSQL_PORT,
+          username: config.MYSQL_USER,
+          password: config.MYSQL_PASSWORD,
+          database: config.MYSQL_DATABASE,
+          entities: [User],
+          synchronize: true
+        }),
+      ],
       providers: [AppService]
     }).compile();
 
--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,18 +1,48 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { UserController } from "./user.controller";
 
+import config from 'src/config';
+import { User } from "./user.entity";
+import { UserService } from "./user.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import * as _ from 'lodash';
+
 describe("User Controller", () => {
   let controller: UserController;
+  let userService: UserService;
 
   beforeEach(async () => {
+    
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [UserController]
+      controllers: [UserController],
+      imports: [
+        TypeOrmModule.forRoot({
+          type: "mysql",
+          host: config.MYSQL_HOST,
+          port: config.MYSQL_PORT,
+          username: config.MYSQL_USER,
+          password: config.MYSQL_PASSWORD,
+          database: config.MYSQL_DATABASE,
+          entities: [User],
+          synchronize: true
+        }),
+        TypeOrmModule.forFeature([User])
+      ],
+      providers: [UserService],
     }).compile();
 
     controller = module.get<UserController>(UserController);
+    userService = module.get<UserService>(UserService);
   });
 
-  it("should be defined", () => {
-    expect(controller).toBeDefined();
+  it("should be an array", done => {
+    expect.assertions(1);
+    controller.findAllUsers().then(res => {
+      expect(_.isArray(res)).toBeTruthy();
+      done();
+    })
+    .catch(err => {
+      done(err);
+    })
   });
 });
