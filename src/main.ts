6c9a0f019617679d27fb611b51df5ddc2b2cb610
Rodchananat K <rod8711@gmail.com>
2020-02-05 11:48:47 +0700

    fix: fix CORS preflight request

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from "./app.module";
 import { DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
 import { Response, Request } from "express";
+import * as cors from 'cors';
 
 import config from 'src/config';
 
@@ -10,6 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   if (config.ALLOW_CORS === "true") {
+    app.use(cors());
     app.use((req: Request, res: Response, next) => {
       res.setHeader('Access-Control-Allow-Credentials', 'true');
       res.setHeader('Access-Control-Allow-Origin', config.CORS_HOST);
