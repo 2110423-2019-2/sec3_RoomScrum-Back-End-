8dfffea55fe5b7adecc469c4e4066077ddede303
Rodchananat K <rod8711@gmail.com>
2020-02-07 15:54:24 +0700

    ci(global): build script

---
+++ build.sh
@@ -0,0 +1,11 @@
+echo "$KEY" >> private_key.cer;
+chmod 600 private_key.cer;
+
+mkdir -p ~/Projects/$APPNAME-$BRANCH &&
+cd ~/Projects/$APPNAME-$BRANCH &&
+source ~/.bashrc &&
+git checkout $BRANCH && git pull origin $BRANCH
+nvm use --lts &&
+yarn &&
+yarn build >> build.log && 
+pm2 restart $APPNAME-$BRANCH
