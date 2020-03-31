2530821a7f77cc1a629293bdb3c807cbf58411bf
Flap <flap.atyz@gmail.com>
2020-03-31 23:43:08 +0700

    feat(review): wip on review service

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Post, Req, Get, Param } from '@nestjs/common';
 import { Review } from 'src/entity/review.entity';
 import { ReviewService } from './review.service';
+import { UserService } from 'src/user/user.service';
 
 @Controller('review')
 export class ReviewController {
@@ -11,19 +12,13 @@ export class ReviewController {
 
     @Get('of-user/:id')
     async getReviewByTargetId(@Req() req): Promise<Review[]> {
-        return [{
-            reviewId: 1, reviewerId: 1, targetId: 2,
-            timeStamp: new Date(), description: "verygood"
-        }, {
-                reviewId: 2, reviewerId: 3, targetId: 2,
-                timeStamp: new Date(), description: "very suck he's so horny"
-            }];
+        
+        return this.reviewService.getReviewByTargetId();
     }
 
     @Get(':id')
     getReviewById(@Param('id') id): Review {
-        return { reviewId:10, reviewerId:1, targetId:2, 
-            timeStamp: new Date(), description:"verygood"};
+        return this.reviewService.getReviewById();
     }
 
     @Post()
--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -1,8 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm';
+import { Review } from 'src/entity/review.entity'
+
 
 @Injectable()
 export class ReviewService {
-    contructor (
-
+    constructor (
+        @InjectRepository(Review)
+        private readonly reviewRepository: Repository<Review>
     ) {}
+
+    async getReviewByTargetId(): Promise<Review[]> {
+        return [{
+            reviewId: 1, reviewerId: 1, targetId: 2,
+            timeStamp: new Date(), description: "verygood"
+        }, {
+            reviewId: 2, reviewerId: 3, targetId: 2,
+            timeStamp: new Date(), description: "very suck he's so horny"
+        }];
+        return ;
+    }
+
+    getReviewById(): Review {
+        return {
+            reviewId: 10, reviewerId: 1, targetId: 2,
+            timeStamp: new Date(), description: "verygood"
+        };
+        return ;
+    }
 }
