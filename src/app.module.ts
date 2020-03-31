28bb501a0dd62ff0945444fde16934577c78e124
Flap <flap.atyz@gmail.com>
2020-03-31 21:15:32 +0700

    feat(review): add review to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,6 +15,7 @@ import { NotificationModule } from "./notification/notification.module";
 import { Notification, EventInviteInfo, BandInviteInfo, EventStateUpdateInfo } from "./entity/notification.entity";
 import { ReportModule } from './report/report.module';
 import { Report } from "./entity/report.entity";
+import { Review } from "src/entity/review.entity";
 
 @Module({
   imports: [
@@ -27,7 +28,7 @@ import { Report } from "./entity/report.entity";
       password: config.MYSQL_PASSWORD,
       database: config.MYSQL_DATABASE,
       entities: [User, Event, Application, Report,
-        Notification, EventInviteInfo, BandInviteInfo, EventStateUpdateInfo,
+        Notification, EventInviteInfo, BandInviteInfo, EventStateUpdateInfo, Review
       ],
       synchronize: true,
       // logging: true,
